75543d9797cf814f698936873115909119ad411f
Steve <sbarry50@gmail.com>
2019-05-01 10:46:31 -0400

    Update README.md demo link

--- demo/App.vue
+++ demo/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="demo-container">
+    <h1>Vue Business Hours</h1>
+    <p>Vue component for setting business hours in an administration panel.</p>
+    <a href="https://github.com/sbarry50/vue-business-hours">Github</a>
     <div class="demo-component">
-      <h1>Business Hours</h1>
+      <h2>Business Hours</h2>
       <business-hours :days="demoDays" :time-increment="15"></business-hours>
     </div>
     <div class="demo-component">
-      <h1>Holiday Hours</h1>
+      <h2>Holiday Hours</h2>
       <business-hours
         :days="demoHolidays"
         name="holidayHours"
@@ -15,7 +18,7 @@
       ></business-hours>
     </div>
     <div class="demo-component">
-      <h1>Business Hours with Errors</h1>
+      <h2>Business Hours with Errors</h2>
       <business-hours :days="demoDaysErrors" name="specialHours" color="#e06c00"></business-hours>
     </div>
   </div>
